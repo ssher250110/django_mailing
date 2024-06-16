6be8a76881d068909ebc559f1d7ae3ae25275a58
ssher <ssher250110@yandex.ru>
2024-06-16 18:48:40 +0300

    added static and media

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,4 +8,4 @@ urlpatterns = [
     path("", include("mailing.urls", namespace="mailing")),
     path("users/", include("users.urls", namespace="users")),
     path("blog/", include("blog.urls", namespace="blog")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
