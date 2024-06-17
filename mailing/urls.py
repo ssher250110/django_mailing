41610cb5cdc231fcdabc8438515ec00b0817d938
ssher <ssher250110@yandex.ru>
2024-06-17 15:52:45 +0300

    patterns view-urls client

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path
+
 from mailing.apps import MailingConfig
+from mailing.views import ClientListView, ClientCreateView, ClientDetailView, ClientUpdateView, ClientDeleteView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
-
+    path("client/create/", ClientCreateView.as_view(), name="client-create"),
+    path("client/", ClientListView.as_view(), name="client-list"),
+    path("client/<int:pk>/", ClientDetailView.as_view(), name="client-detail"),
+    path("client/<int:pk>/update/", ClientUpdateView.as_view(), name="client-update"),
+    path("client/<int:pk>/delete/", ClientDeleteView.as_view(), name="client-delete"),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-# Create your views here.
+from mailing.models import Client
+
+
+class ClientCreateView(CreateView):
+    model = Client
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientDetailView(DetailView):
+    model = Client
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
