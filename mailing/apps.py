b863b4860600bb608b0ed1b5693d4add69fe5a24
ssher <ssher250110@yandex.ru>
2024-06-19 00:36:16 +0300

    added def ready run scheduler

--- mailing/apps.py
+++ mailing/apps.py
@@ -5,3 +5,7 @@ class MailingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mailing"
     verbose_name = "Рассылки"
+
+    # def ready(self):
+    #     from mailing.services import start_send_mailing
+    #     start_send_mailing()
