bc078020cb75bf9837981a16819927e23e7698ee
ssher <ssher250110@yandex.ru>
2024-06-22 18:30:09 +0300

    delete def ready apps.py

--- mailing/apps.py
+++ mailing/apps.py
@@ -5,7 +5,3 @@ class MailingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mailing"
     verbose_name = "Рассылки"
-
-    # def ready(self):
-    #     from mailing.services import start_send_mailing
-    #     start_send_mailing()
