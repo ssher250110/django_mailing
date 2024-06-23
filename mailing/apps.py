f2c45cc606ae63e7598f8108f536ecb2b80b7aea
ssher <ssher250110@yandex.ru>
2024-06-23 23:38:26 +0300

    delete func ready apscheduler in apps.py

--- mailing/apps.py
+++ mailing/apps.py
@@ -7,8 +7,3 @@ class MailingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mailing"
     verbose_name = "Рассылки"
-
-    def ready(self):
-        from mailing.services import filters_and_sorted_mailing_by_condition
-        sleep(2)
-        filters_and_sorted_mailing_by_condition()
