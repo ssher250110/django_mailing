35803810b4500644e1530cadd64cbf9a6e1a1e84
ssher <ssher250110@yandex.ru>
2024-06-23 20:10:27 +0300

    added run apscheduler command from terminal

---
+++ mailing/management/commands/runapscheduler.py
@@ -0,0 +1,25 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.conf import settings
+from django.core.management import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+
+from mailing.services import filters_and_sorted_mailing_by_condition
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            filters_and_sorted_mailing_by_condition,
+            trigger=CronTrigger(second="*/10"),
+            id="filters_and_sorted_mailing_by_condition",
+            max_instances=1,
+            replace_existing=True,
+        )
+        try:
+            scheduler.start()
+        except KeyboardInterrupt:
+            scheduler.shutdown()
