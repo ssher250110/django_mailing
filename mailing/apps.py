6b30bd5c991c4f52a363297be28da24c06013f6d
ssher <ssher250110@yandex.ru>
2024-06-23 20:00:02 +0300

    added func ready run django_apscheduler

--- mailing/apps.py
+++ mailing/apps.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from django.apps import AppConfig
 
 
@@ -5,3 +7,8 @@ class MailingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mailing"
     verbose_name = "Рассылки"
+
+    def ready(self):
+        from mailing.services import filters_and_sorted_mailing_by_condition
+        sleep(2)
+        filters_and_sorted_mailing_by_condition()
