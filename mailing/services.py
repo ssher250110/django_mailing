220a849dd97e44ec8087759fc73ba407f3ea7fc0
ssher <ssher250110@yandex.ru>
2024-06-23 19:54:14 +0300

    added func send_mailing and filters_and_sorted_mailing_by_condition

---
+++ mailing/services.py
@@ -0,0 +1,56 @@
+# from datetime import datetime
+#
+# import pytz
+# from apscheduler.schedulers.background import BackgroundScheduler
+# from django.conf import settings
+# from django.core.mail import send_mail
+#
+# from mailing.models import Mailing, LoggingMailing
+#
+#
+import smtplib
+from datetime import datetime
+
+import pytz
+from django.conf import settings
+from django.core.mail import send_mail
+
+from mailing.models import LoggingMailing, Mailing
+
+
+def send_mailing(mailing):
+    try:
+        response_server = send_mail(
+            subject=mailing.message.subject,
+            message=mailing.message.body,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[client.email for client in mailing.clients.all()],
+            fail_silently=False,
+        )
+        logging_mailing = LoggingMailing.objects.create(mailing=mailing, status_attempt=True, response=response_server)
+        logging_mailing.save()
+    except smtplib.SMTPException as e:
+        logging_mailing = LoggingMailing.objects.create(mailing=mailing, status_attempt=False, response=e)
+        logging_mailing.save()
+
+
+def filters_and_sorted_mailing_by_condition():
+    zone = pytz.timezone(settings.TIME_ZONE)
+    current_datetime = datetime.now(zone)
+    mailings = Mailing.objects.filter(is_active=True).filter(status_mailing__in=["создана", "запущена"])
+    for mailing in mailings:
+        logging_mailing = LoggingMailing.objects.filter(mailing=mailing).order_by("-last_attempt_mailing").first()
+        if logging_mailing.exists():
+            if current_datetime > logging_mailing.last_attempt_mailing:
+                if mailing.period == "раз в день":
+                    if (current_datetime - logging_mailing.last_attempt_mailing).days >= 1:
+                        send_mailing(mailing)
+                if mailing.period == "раз в неделю":
+                    if (current_datetime - logging_mailing.last_attempt_mailing).days >= 7:
+                        send_mailing(mailing)
+                if mailing.period == "раз в месяц":
+                    if (current_datetime - logging_mailing.last_attempt_mailing).days >= 30:
+                        send_mailing(mailing)
+        send_mailing(mailing)
+        mailing.status_mailing = "запущена"
+        mailing.save()
