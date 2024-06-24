04cb0202de161c734508b710e5825ffff554ba7b
ssher <ssher250110@yandex.ru>
2024-06-24 03:41:07 +0300

    added path redirect and login_url

--- config/settings.py
+++ config/settings.py
@@ -101,6 +101,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+LOGIN_URL = 'users:login'
+
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
