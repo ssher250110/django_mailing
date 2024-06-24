bf600777a758e8add5db8f78bf7b99683ebb4ff4
ssher <ssher250110@yandex.ru>
2024-06-25 01:07:24 +0300

    added blog views, urls, templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path
+
 from blog.apps import BlogConfig
+from blog.views import BlogListView, BlogDetailView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-
+    path('blog', BlogListView.as_view(), name='blog'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog-info'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from blog.models import Blog
+
+
+class BlogListView(ListView):
+    model = Blog
+    extra_context = {
+        "Blog_page": "Блог",
+        "title": "Блог",
+        "Not_body": "Содержимое отсутствует",
+        "View": "Посмотреть",
+    }
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    extra_context = {
+        "Blog_page": "Блог",
+        "Not_body": "Содержимое отсутствует",
+        "Data_creation": "Дата публикации",
+        "Data_count_view": "Количество просмотров",
+    }
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.view_count += 1
+        self.object.save()
+        return self.object
