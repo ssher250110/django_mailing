91b745c68e2324ab929322378e2008c942c1ee7f
ssher <ssher250110@yandex.ru>
2024-06-24 17:37:39 +0300

    editing field on exclude

--- mailing/forms.py
+++ mailing/forms.py
@@ -17,16 +17,16 @@ class StyleFormMixin:
 class ClientForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Client
-        fields = "__all__"
+        exclude = ("owner",)
 
 
 class MessageForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Message
-        fields = "__all__"
+        exclude = ("owner",)
 
 
 class MailingForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Mailing
-        fields = "__all__"
+        exclude = ("owner",)
