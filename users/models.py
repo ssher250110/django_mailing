93f766103bce8cf639ca44d0ddbf51c259704027
ssher <ssher250110@yandex.ru>
2024-06-24 03:21:01 +0300

    added field token and func str

--- users/models.py
+++ users/models.py
@@ -11,9 +11,13 @@ class User(AbstractUser):
     country = models.CharField(max_length=85, **NULLABLE, verbose_name="Страна", help_text="Введите название страны")
     avatar = models.ImageField(upload_to="users/avatars/", **NULLABLE, verbose_name="Аватар",
                                help_text="Загрузите аватар")
+    token = models.CharField(max_length=100, **NULLABLE, verbose_name='Токен')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
 
     class Meta:
         verbose_name = "Пользователь"
