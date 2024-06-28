0741bcf61be340bd5f1ecf99f9748a86710bf990
ssher <ssher250110@yandex.ru>
2024-06-28 20:22:04 +0300

    added settings redis

--- config/settings.py
+++ config/settings.py
@@ -115,3 +115,13 @@ EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', False) == 'True'
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CACHE_ENABLED = os.getenv('CACHE_ENABLED', False) == 'True'
+
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': os.getenv('REDIS'),
+        }
+    }
