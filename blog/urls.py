1e84d51130e322b481e00276435b875a771ebf99
ssher <ssher250110@yandex.ru>
2024-06-28 20:30:36 +0300

    added cached app blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from blog.apps import BlogConfig
 from blog.views import BlogListView, BlogDetailView
@@ -6,6 +7,6 @@ from blog.views import BlogListView, BlogDetailView
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('blog', BlogListView.as_view(), name='blog'),
-    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog-info'),
+    path('blog', cache_page(60)(BlogListView.as_view()), name='blog'),
+    path('blog/<int:pk>/', cache_page(60)(BlogDetailView.as_view()), name='blog-info'),
 ]
